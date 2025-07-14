1be1c43a6c3e463412565a41b247a11e3e27a9f7
Ashley Elliott <111938591+spaceashley@users.noreply.github.com>
2025-07-14 16:22:50 -0500

    Update foot-icons.js

--- includes/foot-icons.js
+++ includes/foot-icons.js
@@ -5,4 +5,4 @@ document.write("</ul>");
 document.write("<ul class='copyright'>");
 document.write("<li><strong><font color='#589FCE'>  elliottashley99 AT gmail.com</font></strong></li>");
 document.write("<li>&copy; Ashley Elliott</li>");
-document.wirte("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
+document.write("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
