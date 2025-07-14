2075eac36c747d44ea6aa9b5d33440f077712bd2
Ashley Elliott <111938591+spaceashley@users.noreply.github.com>
2025-07-14 16:15:31 -0500

    Update foot-icons.js

--- includes/foot-icons.js
+++ includes/foot-icons.js
@@ -3,6 +3,6 @@ document.write("<li><a href='https://github.com/spaceashley' class='icon brands 
 document.write("<li><a href='https://www.linkedin.com/in/ashleyelliott0521/' class='icon brands fa-linkedin'><span class='label'>LinkedIn</span></a></li>");
 document.write("</ul>");
 document.write("<ul class='copyright'>");
-document.write("<li><strong><font color='#589FCE'>&#127817;  elliottashley99 AT gmail.com</font></strong></li>");
+document.write("<li><strong><font color='#589FCE'>  elliottashley99 AT gmail.com</font></strong></li>");
 document.write("<li>&copy; Ashley Elliott</li>");
 document.wirte("<li>Design: <a href='https://html5up.net'>HTML5 UP</a></li>");
